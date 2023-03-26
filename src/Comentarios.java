a2a716d362d4ca3a207f6bf6c15c3c58e5de76b3
mokshano11 <mateusggruber@gmail.com>
2023-03-26 14:58:33 -0400

    JAVA DOCS 2

--- src/Comentarios.java
+++ src/Comentarios.java
@@ -2,6 +2,9 @@ public class Comentarios {
     public static void main(String[] args) {   
         // ola, eu sou um comentario em uma unica linha
 }
+/** este metodo foi elaborado as pressas, por isso eu abreviei o nome das variaveis
+ * mas olha, ele tem uma finalidade de somar, multiplicar dois numeros
+*/
     public int somaMultiplica (int n, int x, String m){
         int r = 0; // r e igual ao resultado
         if (m == "M"){ // M = multiplicacao
@@ -15,7 +18,7 @@ public class Comentarios {
         return r;
 
     }
-
+//** este metodo nao segue os parametros de boas praticas na programacao */
 }
     
 
