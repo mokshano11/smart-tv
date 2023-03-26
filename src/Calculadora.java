79658d807154bb4fd99bc266a9c4828059a7e67e
mokshano11 <mateusggruber@gmail.com>
2023-03-26 14:49:10 -0400

    JAVA DOCS 1

---
+++ src/Calculadora.java
@@ -0,0 +1,22 @@
+/**
+ *  <h1>Calculadora</h1>
+ *  A calculadora realiza operacoes matematicas entre numeros inteiros
+ *  <p>
+ *  <b>Note:</b> Leia atentamente a documentacao desta classe
+ *  para desfrutar dos recursos oferecidos pelo autor
+ * 
+ *  @author Mateus Geovani Gruber
+ *  @version 1.0
+ *  @since 1995
+*/
+public class Calculadora {
+/**
+ *  Este metodo e utilizado para somar dois numeros inteiros
+ *  @param numeroUm este e o primeiro parametro do metodo
+ *  @param numeroDois este e o segundo parametro do metodo
+ *  @return int o resultado desse metodo e a soma dos dois numeros 
+ */
+    public int somar(int numeroUm, int numeroDois) {
+        return numeroUm + numeroDois;
+    }
+}
---
+++ src/Comentarios.java
@@ -0,0 +1,21 @@
+public class Comentarios {
+    public static void main(String[] args) {   
+        // ola, eu sou um comentario em uma unica linha
+}
+    public int somaMultiplica (int n, int x, String m){
+        int r = 0; // r e igual ao resultado
+        if (m == "M"){ // M = multiplicacao
+            r = n * x;
+
+        }else{
+            // se nao soma mesmo
+            r = n + x;
+
+        }
+        return r;
+
+    }
+
+}
+    
+
