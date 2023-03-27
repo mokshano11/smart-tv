06da6373e4c3614ff835af2f5a6e3cbd4fd8ccaa
mokshano11 <mateusggruber@gmail.com>
2023-03-27 00:44:46 -0400

    terminal e argumentos 5

--- src/AboutMe.java
+++ src/AboutMe.java
@@ -3,25 +3,28 @@ import java.util.Scanner;
 
 public class AboutMe {
    public static void main(String[] args) {
-      // criando objeto scanner
-       Scanner scanner = new Scanner(System.in).useLocale(Locale.US); 
-
-       System.out.println("digite seu nome");
-      String nome = scanner.next();
       
-       System.out.println("Digite seu sobrenome");
-      String sobrenome = scanner.next();
+      // criando objeto scanner
 
-       System.out.println("Digite sua idade");
-      int idade = scanner.nextInt(); 
+      try (Scanner scanner = new Scanner(System.in).useLocale(Locale.US)) {
+         System.out.println("digite seu nome");
+         String nome = scanner.next();
+         
+         System.out.println("Digite seu sobrenome");
+         String sobrenome = scanner.next();
 
-       System.out.println("Digite sua altura ");
-      double altura = scanner.nextDouble(); 
+         System.out.println("Digite sua idade");
+         int idade = scanner.nextInt(); 
 
-    // imprimindo dados inseridos pelo usuario
-System.out.println("Ola, me chamo " +  nome   + " " + sobrenome);
-System.out.println("Tenho " + idade + " anos ");
-System.out.println("Minha altura e " + altura + "cm ");
+         System.out.println("Digite sua altura ");
+         double altura = scanner.nextDouble(); 
+
+   // imprimindo dados inseridos pelo usuario
+
+         System.out.println("Ola, me chamo " +  nome   + " " + sobrenome);
+         System.out.println("Tenho " + idade + " anos ");
+         System.out.println("Minha altura e " + altura + "cm ");
+      }
 
    }
      //** os argumentos comecam com indice "0"
