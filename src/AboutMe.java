440ff52b2007468a057c66e5e5df6d1488c1fa6b
mokshano11 <mateusggruber@gmail.com>
2023-03-27 00:40:05 -0400

    terminal e argumentos 3 e 4

--- src/AboutMe.java
+++ src/AboutMe.java
@@ -1,15 +1,32 @@
+import java.util.Locale;
+import java.util.Scanner;
+
 public class AboutMe {
    public static void main(String[] args) {
-    // os argumentos comecam com indice "0"
-    String nome = args [0];
-    String sobrenome = args [1];
-    int idade = Integer.valueOf(args [2]);
-    double altura = Double.valueOf(args [3]);
+      // criando objeto scanner
+       Scanner scanner = new Scanner(System.in).useLocale(Locale.US); 
 
+       System.out.println("digite seu nome");
+      String nome = scanner.next();
+      
+       System.out.println("Digite seu sobrenome");
+      String sobrenome = scanner.next();
+
+       System.out.println("Digite sua idade");
+      int idade = scanner.nextInt(); 
+
+       System.out.println("Digite sua altura ");
+      double altura = scanner.nextDouble(); 
+
+    // imprimindo dados inseridos pelo usuario
 System.out.println("Ola, me chamo " +  nome   + " " + sobrenome);
 System.out.println("Tenho " + idade + " anos ");
 System.out.println("Minha altura e " + altura + "cm ");
 
    }
-    
+     //** os argumentos comecam com indice "0"
+   // String nome = args [0];
+   // String sobrenome = args [1];
+   // int idade = Integer.valueOf(args [2]);
+    //double altura = Double.valueOf(args [3]);
 }
